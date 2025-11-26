f9aaa4d56cfd3c7e768d866abedb5596eeffdf46
Artus-lm <artusklemm@gmail.com>
2025-11-26 14:29:56 +0200

    somethingsomething

---
+++ Kodu/javascript.js
@@ -0,0 +1,14 @@
+var mydata = JSON.parse("../autoupdate/mangud.json");
+
+console.log(mydata[0].date);
+/*
+function createTable() {
+    var body = document.getElementById("scoreboard");
+    var tbl = document.createElement('table');
+    tbl.class = "scoreboard-table";
+    var tbdy = document.createElement("tbody")
+    for (var i = 0; i < 5; i++) {
+
+    }
+}
+console.log(getData)
