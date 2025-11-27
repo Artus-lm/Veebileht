73225aa75c5a91e16ce5cd080e5061e0c2ce4dee
Artus-lm <artusklemm@gmail.com>
2025-11-27 02:10:37 +0200

    prettified/rewrote autoupdate

--- Kodu/autoupdate/auto-update.py
+++ Kodu/autoupdate/auto-update.py
@@ -1,122 +1,134 @@
-import bs4, requests
-
-#
-#  ---- Disclaimer ----
-# File needs minor formating after program run.
-# You need to remove last coma after the "}"
-#
+import bs4, requests, json
 
 # --- Options ---
 
-team = "Structura/Tartu vald "                  # team name + ' ' for specific team or None for all teams
-save_to = "autoupdate/mangud.json"              # save file path
+wanted_team = "Structura/Tartu vald"            # team name for specific team or None for all teams
+save_to = "./Kodu/autoupdate/mangud.json"        # save file path
 site_url = "https://evf-web.dataproject.com/CompetitionMatches.aspx?ID=84&PID=126"
 
 
 # --- Functions ---
 
-def find_length(list, date):
+def findLength(list, date):
     count = 0
     for i, value in enumerate(list):
-        if value == date:
-            count += 1
-            match count:
-                case 3:
-                    first = i
-                case 4:
-                    secnd = i - first
-                    return i + secnd      
-    return -1
+        if (value == date) & (i != 0):
+            return i
+    print("Error, could not find second date in findLength")
 
-def checkIfDate(item, date_of_reference, current_date_counter, current_counter):
-    if item == date_of_reference:
-        current_date_counter += 1
-        if current_date_counter < 4:
-            current_counter = -1
-    return current_date_counter, current_counter
+
+def getLoc(list, reference_date):
+    location = ""
+
+    for i in range(10):
+        location += list[0]
+        list = list[1:]
+
+        if list[0] == reference_date:
+            return list, location
+        location += " "
+
+    print("Error, did not find 2nd reference date in getLoc")
+    
+
+def getTeam1(list):
+    team = ""
+    list = list[3:]
+    for i in range(10):
+        team += list[0]
+        list = list[1:]
+        if list[0] == "-":
+            return list, team
+        try: 
+            int(list[0])
+            return list, team      
+        except:
+            team += " "
+    print('Error, did not encounter number or "-" in getTeam1')
+        
+
+def getTeam2(list, reference_date):
+    team = ""
+    for i in range(10):
+        team += list[0]
+        list = list[1:]
+        if list[0] == reference_date:
+            return list, team
+        team += " "
+    print("Error, did not encounter reference date in getTeam2")
+
+
+def areAnnounced(list):
+    if list[0] != "Finaal":
+        return False, True, True, True, True
+    
+    finaal = False
+    thirdfourth = False
+    pool = False
+    veerand = False
+
+    if list[16] == "No":                                #if first statement is true all matches must be false,
+        return True, finaal, thirdfourth, pool, veerand #if the second is true the first couldnt be so first match must be true
+    if list[11] == "No":                                # and all others false and so on
+        veerand = True
+        return True, finaal, thirdfourth, pool, veerand
+    if list[6] == "No":
+        pool = True
+        return True, finaal, thirdfourth, pool, veerand
+    if list[1] == "No":
+        thirdfourth = True
+        return True, finaal, thirdfourth, pool, veerand
+    print("Error, could not identify if the games are announced in areAnnounced")
+
+# --- Main Function ---
 
 def dataExtract(unextracted_list):
     extracted = []
+    plofs, finaal, thirdfourth, pool, veerand = areAnnounced(unextracted_list)
+
+    if plofs:
+        if not veerand:
+            unextracted_list = [] 
+        elif not pool:
+            unextracted_list = unextracted_list[15:] 
+        elif not thirdfourth:
+            unextracted_list = unextracted_list[10:] 
+        elif not finaal:
+            unextracted_list = unextracted_list[5:] 
+
+
     while unextracted_list != []:
-        temp = []
-        counter = 0
-        date_counter = 0
-        name_compiler = ""
-        venue_compiler = ""
+        
+        if plofs:
+            unextracted_list[1:]
+        
         reference_date = unextracted_list[0]
-        for i in unextracted_list:
-            date_counter, counter = checkIfDate(i, reference_date, date_counter, counter)
-            counter += 1
-            match date_counter, counter:
-                case 1, 0 | 2:
-                    temp.append(i)
-                case 1, 1:
-                    pass
-                case 1, _:
-                    venue_compiler += i + " "
-                case 2, 0:
-                    temp.append(venue_compiler)
-                case 3, 0 | 1 | 2:
-                    pass
-                case 3, _:
-                    try:
-                        temp.append(int(i))
-                    except:
-                        if i == '-':
-                            temp.append(name_compiler)
-                            name_compiler = ""
-                        else:
-                            name_compiler += i + " "
-                case 4, _:
-                    temp.append(name_compiler)
-                    extracted.append(temp[:])
-                    unextracted_list = unextracted_list[find_length(unextracted_list, reference_date):]
-                    break
-    return extracted
-# could probably merge these two into one function and extract subfunctions out to make it more readable
-def playoffsExtract(ploffs):
-    extracted = []
-    while ploffs != []:
-        if ploffs[1] == "No":
-            extracted.append([ploffs[0], "TBA"])
-            ploffs = ploffs[5:]
-        else:
-            temp = [ploffs[0]]
-            counter = -1
-            date_counter = 0
-            name_compiler = ""
-            venue_compiler = ""
-            reference_date = ploffs[1]
-            for i in ploffs:
-                if i == reference_date:
-                    date_counter += 1
-                    counter = -1
-                counter += 1
-                match date_counter, counter:
-                    case 1, 0 | 2:
-                        temp.append(i)
-                    case 1, 1:
-                        pass
-                    case 1, _:
-                        venue_compiler += i + " "
-                    case 2, 0:
-                        temp.append(venue_compiler)
-                    case 3, 0 | 1 | 2:
-                        pass
-                    case 3, _:
-                        try:
-                            temp.append(int(i))
-                        except:
-                            if i == '-':
-                                temp.append(name_compiler)
-                                name_compiler = ""
-                            else:
-                                name_compiler += i + " "
-                    case 4, length:
-                        temp.append(name_compiler)
-                        extracted.append(temp[:])
-                        ploffs = ploffs[find_length(ploffs, reference_date)+1:]
-                        break
+
+        temp = {"date" : reference_date, "time" : unextracted_list[2]}
+        unextracted_list = unextracted_list[3:]
+        
+
+        unextracted_list, temp["location"] = getLoc(unextracted_list, reference_date)
+        unextracted_list = unextracted_list[3:]
+
+        names_length = findLength(unextracted_list, reference_date)
+        unextracted_list, temp["home"] = getTeam1(unextracted_list)
+
+        if unextracted_list[0] == "-":
+            temp["state"] = "TB"
+            unextracted_list = unextracted_list[1:]
+        else: 
+            temp["home_points"] = unextracted_list[0]
+            temp["away_points"] = unextracted_list[2]
+            temp["state"] = "Done"
+            unextracted_list = unextracted_list[3:]
+        
+        unextracted_list, temp["away"] = getTeam2(unextracted_list, reference_date)
+        unextracted_list = unextracted_list[names_length:]
+        if (temp["home"] == wanted_team) | (temp["away"] == wanted_team) | (wanted_team == None):
+            extracted.append(temp)
+
+    pass
     return extracted
 
 
@@ -126,76 +138,18 @@ def playoffsExtract(ploffs):
 response = requests.get(site_url, headers={'User-Agent': 'Mozilla/5.0'})
 soup = bs4.BeautifulSoup(response.text, features="html.parser")
 
-site_text = soup.body.get_text(' ', strip=True)    # https://stackoverflow.com/questions/69593352/how-to-get-all-copyable-text-from-a-web-page-python
+site_text = soup.body.get_text(' ', strip=True)    # https://stackoverflow.com/questions/69593352/how-to-get-all-copyable-location-from-a-web-page-python
 
 data = site_text[site_text.find("Matches Põhiturniir")+20:(site_text.find("Play-Off Play-Off") - len(site_text))]  # removes all text that isnt needed
 playoffs = site_text[site_text.find("Play-Off Play-Off")+26:-77]
 
 extracted = dataExtract(data.split())
-extracted_playoffs = playoffsExtract(playoffs.split())
+extracted_playoffs = dataExtract(playoffs.split())
 
 
 # --- Saving ---
 
+to_write = extracted + extracted_playoffs
 
-json_file = open(save_to, mode="w", encoding="UTF-8")
-
-json_file.write("[")
-for i in extracted:
-    if (team == None) | (team in i):
-        if len(i) == 7:
-            json_file.write(f"""
-    {{
-        "date": "{i[0]}",
-        "time": "{i[1]}",
-        "location": "{i[2]}",
-        "home_points": "{i[3]}",
-        "home": "{i[4]}",
-        "away_points": "{i[5]}",
-        "away": "{i[6]}",
-        "state": "Done"
-    }},
-""")
-        else:
-            json_file.write(f"""
-    {{
-        "date": "{i[0]}",
-        "time": "{i[1]}",
-        "location": "{i[2]}",
-        "home": "{i[3]}",
-        "away": "{i[4]}",
-        "state": "TB"
-    }},
-""")
-
-for i in extracted_playoffs:
-    if i[1] == "TBA":
-        pass
-    elif len(i) == 7:
-        json_file.write(f"""
-    {{
-        "date": "{i[0]}",
-        "time": "{i[1]}",
-        "location": "{i[2]}",
-        "home_points": "{i[3]}",
-        "home": "{i[4]}",
-        "away_points": "{i[5]}",
-        "away": "{i[6]}",
-        "state": "Done"
-    }},
-""")
-    else:
-        json_file.write(f"""
-    {{
-        "date": "{i[0]}",
-        "time": "{i[1]}",
-        "location": "{i[2]}",
-        "home": "{i[3]}",
-        "away": "{i[4]}",
-        "state": "TB"
-    }},
-""")
-
-json_file.write("]")
-
-json_file.close()
+with open(save_to, mode="w", encoding="UTF-8") as jfile:
+    json.dump(to_write, jfile, indent=4, ensure_ascii=False)
